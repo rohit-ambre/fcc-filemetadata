92f7a7c3b1de70b2cb54169338f9486ba291b192
Rohit Ambre <rohit003.ambre@gmail.com>
2019-09-15 16:19:43 +0530

    added post file upload route, complete

--- server.js
+++ server.js
@@ -2,22 +2,31 @@
 
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser')
+const multer = require('multer');
 
-// require and use "multer"...
+const upload = multer({ dest: './uploads/' });
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/hello', function (req, res) {
+    res.json({ greetings: "Hello, API" });
 });
+
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+    res.send({ name: req.file.originalname, mimetype: req.file.mimetype, size: req.file.size })
+});
+
+const listener = app.listen(process.env.PORT || 3000, function () {
+    console.log('Node.js listening ...');
+});
+
+module.exports = listener;
